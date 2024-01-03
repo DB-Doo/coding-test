5c8b93cd94bc80df033c739eabf30521d904f310
Dan Brandt <dan.p.brandt@gmail.com>
2024-01-03 15:23:34 -0600

    basic styling and html setup. Started on placeholder array in js, defined variables to be used later, and created pseudo framework

--- Assets/script.js
+++ Assets/script.js
@@ -9,3 +9,32 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+const quizQuestions = [
+    {
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },
+    {
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },{
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },{
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },{
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },{
+        question: "question",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: "correct answer"
+    },
+];
